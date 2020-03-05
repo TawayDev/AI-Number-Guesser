7a93fa3437785d0dd2c2d40a3566e76e7d328c34
Jakub <44374628+itsJeras@users.noreply.github.com>
2020-03-05 21:57:22 +0100

    Update Main.java

--- NumGuessor/src/com/numero/Main.java
+++ NumGuessor/src/com/numero/Main.java
@@ -1,6 +1,6 @@
 package com.numero;
 // I AM FULLY AWARE THAT MY CODE IS SPAGHETTI AND IT COULD BE MADE EASIER AND MORE EFFICIENT BUT I AM NOT THE BEST PROGRAMMER UNDER THE SUN.
-// CODE WRITTEN BY GITHUB USER ITSJERAS.
+// CODE WRITTEN BY GITHUB USER ITSJERAS. (https://github.com/itsJeras/AI-Number-Guesser)
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
